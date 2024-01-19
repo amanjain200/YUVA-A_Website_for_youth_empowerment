e34e9770c5485db833716daf7b8570e3093a8777
amanjain200 <amanjain189200@gmail.com>
2024-01-20 02:04:56 +0530

    some minor changes

--- collabs.js
+++ collabs.js
@@ -5,18 +5,18 @@ const campusBtn = document.querySelectorAll("#campus-join-btn");
 
 collabBtn[0].addEventListener("click", () => {
   console.log("I am in collabBtn");
-  window.open(
-    "https://www.facebook.com",
-    "Join our Creator-collab Program",
-    "width=620,height=490,status=yes,resizable=yes, left=290, top=100"
-  );
+  // window.open(
+  //   "https://www.facebook.com",
+  //   "Join our Creator-collab Program",
+  //   "width=620,height=490,status=yes,resizable=yes, left=290, top=100"
+  // );
 });
 
 campusBtn[0].addEventListener("click", () => {
   console.log("I am in campusBtn");
-  window.open(
-    "https://forms.gle/7X7g7zjxYQK1qJ7b9",
-    "Join our Campus ambassador Program",
-    "width=620,height=490,status=yes,resizable=yes, left=290, top=100"
-  );
+  // window.open(
+  //   "https://forms.gle/7X7g7zjxYQK1qJ7b9",
+  //   "Join our Campus ambassador Program",
+  //   "width=620,height=490,status=yes,resizable=yes, left=290, top=100"
+  // );
 });
